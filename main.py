3de8b90751fd4ae46ef65fe61827f5885308781a
Var-01 <vartikaverma011929@gmail.com>
2023-04-21 14:07:25 +0530

    completing the recommendation part

--- main.py
+++ main.py
@@ -45,4 +45,16 @@ def get_movie_recommendation(name):
         distances, indices = knn.kneighbors(csr_data[movie_idx], n_neighbors = n_movies_to_reccomend + 1)
         rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(), distances.squeeze().tolist())), key = lambda x: x[1])[:0:-1]
         recommend_frame = []
-        
+        for val in rec_movie_indices:
+            movie_idx = final_dataset.iloc[val[0]]['movieId']
+            idx = movies[movies['movieId'] == movie_idx].index
+            recommend_frame.append({'Title':movies.iloc[idx]['title'].values[0], 'Distance':val[1]})
+        df = pd.DataFrame(recommend_frame, index = range(1, n_movies_to_reccomend + 1))
+        x=dict()
+        j=0
+        for i in recommend_frame:
+            x['Title'+str(j)]=i['Title']
+            j+=1
+        return render_template('index.html',movie=x)
+    else:
+        return "No movies found. Please try another movie!"
